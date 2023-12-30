2a073bdf10c46a02c695ed0ad6f416792dd89459
downred <redonqerimi@gmail.com>
2023-12-30 20:59:40 +0100

    make nodes clickable and add functionality to set node edge

--- GUI.py
+++ GUI.py
@@ -1,12 +1,12 @@
 import tkinter as tk
+
+from dijkstra import dijkstra
 from graph import Graph, Node, Edge
 
 def get_node(node_value):
     for node in node_objects.keys():
         if node.value == node_value:
-            print(f"Found node {node} at target position")
-            break
-
+            return node
 
 node_count=0
 node_names="ABCD"
@@ -15,10 +15,42 @@ node_pos_list = []
 node_pos = []
 node_objects = {}
 
+
+def set_edge(node_value):
+    global current_node_pos, node_to_pos
+    current_node = get_node(node_value)
+    for pos in node_pos_list:
+        if pos[0] == current_node:
+            current_node_pos = pos[1]
+
+    node_to = input("Enter a node: ")
+    user_input = int(input("Enter the edge distance: "))
+
+    node_to = get_node(node_to)
+
+    for pos in node_pos_list:
+        if pos[0] == node_to:
+            node_to_pos = pos[1]
+
+    node_objects[current_node].append(Edge(user_input, node_to))
+    node_objects[node_to].append(Edge(user_input, current_node))
+    canvas.create_line(current_node_pos, node_to_pos, fill="black", width=2)
+
+
+
 g = Graph()
+
+g.adjacency_list = node_objects
 
 def on_click(event):
     global node_count, node_names
+
+    for node, (x, y) in node_pos_list:
+        if (x - 12 <= event.x <= x + 12) and (y - 12 <= event.y <= y + 12):
+            print(f"Clicked on node {node} at position ({x}, {y})")
+            # get_node(node.value)
+            set_edge(node.value)
+            break
 
     if node_count < len(node_names):
 
@@ -31,17 +63,19 @@ def on_click(event):
         canvas.create_text(x,y, text=node_names[node_count], fill="black")
         node_count = node_count + 1
         node_pos = [event.x, event.y]
-        node_pos_list.append(node_pos)
+        node_pos_list.append((node, node_pos))
         # canvas.create_line(360, 540, event.x, event.y, fill="black", width=2)
     else:
         print("Node limit reached!")
         # print(node_pos_list)
         # print(str(node_objects[0])) 
-        for node in node_pos_list:
-            for starter_pos in node_pos_list:
-                canvas.create_line(starter_pos, node, fill="black", width=2)
-        get_node("A")
-
+        # for node in node_pos_list:
+        #     for starter_pos in node_pos_list:
+        #         canvas.create_line(starter_pos, node, fill="black", width=2)
+        # get_node("A")
+        #
+        print_node_objects()
+        # dijkstra(g, node_objects[get_node("A")])
         # print(node_A)
 
 
@@ -61,6 +95,14 @@ def main():
 
     root.mainloop()
 
+def print_node_objects(): #this method prints the all the nodes in node_objects and the edges for each node(edges are printed under the node as (distance, node) tuples)
+    for node, edges in node_objects.items():
+        print(f"Node {node}")
+        for edge in edges:
+            print(edge)
+
 if __name__ == "__main__":
     main()
+
+
 
--- graph.py
+++ graph.py
@@ -8,8 +8,14 @@ class Node:
     def __init__(self, value):
         self.value = value
 
+    def __str__(self):
+        return self.value
+
 
 class Edge:
     def __init__(self, distance, node):
         self.distance = distance
         self.node = node
+
+    def __str__(self):
+        return "(" + str(self.distance) + "," + str(self.node.value) + ")"
