fdac422abac0f5ff8d7de5562b0296f20ca10021
downred <redonqerimi@gmail.com>
2023-12-31 22:34:35 +0100

    add button for finding path

--- GUI.py
+++ GUI.py
@@ -9,7 +9,7 @@ def get_node(node_value):
             return node
 
 node_count=0
-node_names="ABCD"
+node_names="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 node_pos_list = []
 node_pos = []
@@ -18,9 +18,10 @@ node_objects = {}
 
 def set_edge(node_value):
     global current_node_pos, node_to_pos
+    canvas.unbind("<Button-1>")
     current_node = get_node(node_value)
     for pos in node_pos_list:
-        if pos[0] == current_node:
+        if pos[0] == current_node: # (nodeA, (x,y))
             current_node_pos = pos[1]
 
     node_to = input("Enter a node: ")
@@ -32,25 +33,26 @@ def set_edge(node_value):
         if pos[0] == node_to:
             node_to_pos = pos[1]
 
+    # nodeA: [Edge(3, nodeB)]
+    # nodeB: [Edge(3, nodeA)]
     node_objects[current_node].append(Edge(user_input, node_to))
     node_objects[node_to].append(Edge(user_input, current_node))
     canvas.create_line(current_node_pos, node_to_pos, fill="black", width=2)
+    canvas.bind("<Button-1>", on_click)
 
 
 
 g = Graph()
 
-g.adjacency_list = node_objects
+
 
 def on_click(event):
     global node_count, node_names
 
     for node, (x, y) in node_pos_list:
         if (x - 12 <= event.x <= x + 12) and (y - 12 <= event.y <= y + 12):
-            print(f"Clicked on node {node} at position ({x}, {y})")
-            # get_node(node.value)
             set_edge(node.value)
-            break
+            return
 
     if node_count < len(node_names):
 
@@ -74,22 +76,38 @@ def on_click(event):
         #         canvas.create_line(starter_pos, node, fill="black", width=2)
         # get_node("A")
         #
-        print_node_objects()
-        # dijkstra(g, node_objects[get_node("A")])
         # print(node_A)
 
 
+def find_shortest_path():
+    print_node_objects()
+    g.adjacency_list = node_objects
+    starting_node = input("Sheno kulmin startues: ")
+
+    dijkstra(g, get_node(starting_node))
+
 def main():
     root = tk.Tk()
-    root.title("Click to place a node")
+    root.title("Shto kulmet e grafit duke klikuar me maus.")
 
-    window_width = 500
-    window_height = 500
+    window_width = 600
+    window_height = 600
     root.geometry(f"{window_width}x{window_height}")
 
+    # Create a frame for the canvas
+    canvas_frame = tk.Frame(root)
+    canvas_frame.pack(side=tk.TOP)
+
     global canvas
-    canvas = tk.Canvas(root, width=window_width, height=window_height)
+    canvas = tk.Canvas(canvas_frame, width=window_width, height=window_height, bg="white")
     canvas.pack()
+
+    # Create a frame for the buttons
+    button_frame = tk.Frame(root)
+    button_frame.pack(side=tk.LEFT)
+
+    button2 = tk.Button(button_frame, text="FIND PATH", command=find_shortest_path)
+    button2.pack(side=tk.LEFT, padx=5, pady=5)
 
     canvas.bind("<Button-1>", on_click)
 
